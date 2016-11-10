ceacd5b785174e615432da6f01599e7bcd141097
ksincennes <kevin.sincennes@farmersedge.ca>
2016-11-10 16:31:38 -0700

    Workingish geojson

--- android/src/main/java/com/airbnb/android/react/maps/AirMapGeoJSON.java
+++ android/src/main/java/com/airbnb/android/react/maps/AirMapGeoJSON.java
@@ -8,29 +8,39 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.geojson.GeoJsonFeature;
 import com.google.maps.android.geojson.GeoJsonLayer;
+import com.google.maps.android.geojson.GeoJsonGeometry;
 
 import java.util.ArrayList;
 import java.util.List;
 import org.json.JSONObject;
+import org.json.JSONArray;
 import org.json.JSONException;
 import android.util.Log;
+
+
+
 
 public class AirMapGeoJSON extends AirMapFeature {
 
     private JSONObject feature;
     private GeoJsonLayer layer;
-    private ReadableMap style;
+    private JSONObject styles;
     private GoogleMap mapObj;
+    private Map<String, GeoJsonPolygonStyle> polyStyles;
+    private Map<String, GeoJsonLineStringStyle> lineStyles;
+    private Map<String, GeoJsonPointStyle> pointStyles;
+    private boolean byProp;
+
 
     public AirMapGeoJSON(Context context) {
         super(context);
     }
 
-    public void setCoordinates(String geoJson) {     
-    Log.w("GEOJSONPARSE", geoJson);
+    public void setGeoJSON(String geoJson) {     
         if (geoJson != "") {
             try{
                 feature = new JSONObject(geoJson);
+                
             } catch(JSONException err){
                 Log.e("GEOJSONPARSE",err.getMessage());
             }
@@ -41,8 +51,27 @@ public class AirMapGeoJSON extends AirMapFeature {
         }
     }
 
-    public void setStyle(ReadableMap style) {
-        style = style;
+    public void setStyles(String style) {
+        if(styles != ""){            
+            styles = new JSONObject(style);
+        }
+        if(layer != null && style.has("name") && style.has("styles")){
+            List<GeoJsonFeature> featureList = layer.getFeatures();
+            String key = style.getString("name")
+            JSONObject styleMap = 
+            for(GeoJsonFeature styledFeature : featureList){
+                if(styledFeature.has(key)){
+                    String value = styledFeature.getString(key)
+                    if(styles.has(value)){
+                        JSONObject style = styles.getJSONObject(value)
+                        if(styledFeature.getString("type").match("Polygon|MultiPolygon|GeometryCollection")){
+
+                            if(style)
+                        } //else if()
+                    }
+                }
+            }
+        }
        /* if (feature != null) {
             GeoJsonPolygonStyle polyStyle = new GeoJsonPolygonStyle()
             feature.getDefault
@@ -74,4 +103,48 @@ public class AirMapGeoJSON extends AirMapFeature {
             layer.removeLayerFromMap();
         }
     }
+
+    public void setFillColor(int color) {
+        this.fillColor = color;
+        if (polygon != null) {
+            polygon.setFillColor(color);
+        }
+    }
+
+    public void setStrokeColor(int color) {
+        this.strokeColor = color;
+        if (polygon != null) {
+            polygon.setStrokeColor(color);
+        }
+    }
+
+    public void setStrokeWidth(float width) {
+        this.strokeWidth = width;
+        if (polygon != null) {
+            polygon.setStrokeWidth(width);
+        }
+    }
+
+    public void setGeodesic(boolean geodesic) {
+        this.geodesic = geodesic;
+        if (polygon != null) {
+            polygon.setGeodesic(geodesic);
+        }
+    }
+
+    public void setZIndex(float zIndex) {
+        this.zIndex = zIndex;
+        if (polygon != null) {
+            polygon.setZIndex(zIndex);
+        }
+    }
+
+    private void generateLineStringStyle(){
+
+    }
+    private void generatePolyStyle(){
+        
+    }
+
+
 }
--- android/src/main/java/com/airbnb/android/react/maps/AirMapGeoJSONManager.java
+++ android/src/main/java/com/airbnb/android/react/maps/AirMapGeoJSONManager.java
@@ -40,13 +40,45 @@ public class AirMapGeoJSONManager extends ViewGroupManager<AirMapGeoJSON> {
         return new AirMapGeoJSON(context);
     }
 
-    @ReactProp(name = "coordinates")
-    public void setCoordinates(AirMapGeoJSON view, String coordinates) {
-        view.setCoordinates(coordinates);
+    @ReactProp(name = "geoJSON")
+    public void setGeoJSON(AirMapGeoJSON view, String geoJSON) {
+        view.setGeoJSON(coordinates);
     }
 
-    @ReactProp(name = "style")
-    public void setStyle(AirMapGeoJSON view,  ReadableMap style) {
-        view.setStyle(style);
+    @ReactProp(name = "styles")
+    public void setStyle(AirMapGeoJSON view,  String style) {
+        view.setStyles(style);
+    }
+
+    @ReactProp(name = "byProp")
+    public void setByProp(AIRMapGeoJSON view, boolean byProp) {
+        view.setByProp(byProp)
+    }
+
+
+    @ReactProp(name = "strokeWidth", defaultFloat = 1f)
+    public void setStrokeWidth(AirMapPolygon view, float widthInPoints) {
+        float widthInScreenPx = metrics.density * widthInPoints; // done for parity with iOS
+        view.setStrokeWidth(widthInScreenPx);
+    }
+
+    @ReactProp(name = "fillColor", defaultInt = Color.RED, customType = "Color")
+    public void setFillColor(AirMapPolygon view, int color) {
+        view.setFillColor(color);
+    }
+
+    @ReactProp(name = "strokeColor", defaultInt = Color.RED, customType = "Color")
+    public void setStrokeColor(AirMapPolygon view, int color) {
+        view.setStrokeColor(color);
+    }
+
+    @ReactProp(name = "geodesic", defaultBoolean = false)
+    public void setGeodesic(AirMapPolygon view, boolean geodesic) {
+        view.setGeodesic(geodesic);
+    }
+
+    @ReactProp(name = "zIndex", defaultFloat = 1.0f)
+    public void setZIndex(AirMapPolygon view, float zIndex) {
+        view.setZIndex(zIndex);
     }
 }
